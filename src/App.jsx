afff5d59d3f406bcb1b6e4a102f796d0f7c5fd86
Aman <rajaman@gmail.com>
2024-12-01 15:55:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { ImageGenerator } from './Components/ImageGenerator'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+            <ImageGenerator/>
+      </div>
+    </>
+  )
+}
+
+export default App
